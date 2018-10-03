b542dabd626743ac2a6d85ceca27ef07e65ec608
Joohyun Lee <dlee110600@gmail.com>
2018-10-03 20:08:30 +0900

    Update train.py

--- keras/train.py
+++ keras/train.py
@@ -1,45 +1,49 @@
-import pdb
 import argparse
 
-from data import *
+from data2 import *
 from unet import *
+
 
 def train(args):
 
     # Data Load
-    trainset = trainGenerator(args)
+    trainset = dataset(args, mode='train')
+    validset = dataset(args, mode='valid')
 
     # Model Load
     model = unet(args)
     model_checkpoint = ModelCheckpoint(args.ckpt_path,
-                                       monitor='loss',verbose=2,
+                                       monitor='val_loss', verbose=2,
                                        save_best_only=True)
-    model.fit_generator(trainset, steps_per_epoch=200, shuffle=True,
-                        epochs=args.epoch,callbacks=[model_checkpoint],
-                        workers=16, use_multiprocessing=True)
-
+    # Model Train
+    model.fit_generator(trainset, steps_per_epoch=500, shuffle=True, epochs=args.epoch,
+                        validation_data=validset, validation_steps=2000,
+                        callbacks=[model_checkpoint], workers=16)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--resume', type=bool, default=False,
                         help='Resume model checkpoint')
-    parser.add_argument('--batch_size', type=int, default=20,
+    parser.add_argument('--batch_size', type=int, default=32,
                         help='batch size.')
     parser.add_argument('--lr', type=float, default=0.0001,
                         help='starting learning_rate')
-    parser.add_argument('--epoch', type=int, default=10,
+    parser.add_argument('--epoch', type=int, default=2,
                         help='number of epochs.')
     parser.add_argument('--data', type=str, default='complete',
                         help='MRI Label data to train')
-    parser.add_argument('--train_root', type=str,
-                        default='../data/train/keras',
-                        help='the directory containing the train dataset.')
-    parser.add_argument('--image_folder', type=str,
-                        default='flair2',
-                        help='the directory containing the trian image dataset.')
-    parser.add_argument('--label_folder', type=str,
-                        default='label2',
-                        help='the directory containing the train label dataset.')
+    parser.add_argument('--image_root', type=str, default='../data/train/keras',
+                        help='the root directory containing the image dataset.')
+    parser.add_argument('--image_folder1', type=str, default='flair1',
+                        help='the directory containing the image dataset(whole)')
+    parser.add_argument('--image_folder2', type=str, default='flair2',
+                        help='the directory containing the image dataset(ratio>n%)')
+    parser.add_argument('--label_root', type=str, default='../data/train/keras',
+                        help='the root directory containing the label dataset.')
+    parser.add_argument('--label_folder1', type=str, default='label1',
+                        help='the directory containing the train label dataset(whole).')
+    parser.add_argument('--label_folder2', type=str, default='label2',
+                        help='the directory containing the train label dataset(ratio>n%).')
     parser.add_argument('--ckpt_path', type=str, default='./checkpoint/unet.hdf5',
                         help='The directory containing the generative image.')
     args = parser.parse_args()
