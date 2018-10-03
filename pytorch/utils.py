606d77de2d4e2c370f19adcca795682ab8f80c5a
Joohyun Lee <dlee110600@gmail.com>
2018-10-04 08:54:38 +0900

    Add files via upload

---
+++ pytorch/utils.py
@@ -0,0 +1,173 @@
+import pdb
+import cv2
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import os
+import sys
+import time
+from torch.autograd import Function
+from torch.nn.functional import softmax
+
+
+_, term_width = os.popen('stty size', 'r').read().split()
+term_width = int(term_width)
+
+TOTAL_BAR_LENGTH = 65.
+last_time = time.time()
+begin_time = last_time
+
+
+def dice_coef(pred, target):
+    smooth = 1.0
+    class_num = pred.shape[1]
+    for i in range(class_num):
+        _pred = pred[:,i,:,:]
+        _target = target[:,i,:,:]
+        _intersection = (_pred * _target).sum()
+        _loss = 1 - ((2.0 * _intersection + smooth) / (_pred.sum() + _target.sum() + smooth))
+        if i == 0:
+            loss = _loss
+        else:
+            loss = loss + _loss
+    loss = loss/class_num
+    return loss
+
+def save_img(args, input, output, input_path, overlap=True):
+    input = (np.array(input.squeeze()).astype(np.float32)) * 255
+    input = np.expand_dims(input, axis=3)
+    input = np.concatenate((input,input,input), axis=3)
+    output = np.array(output.max(1)[1])*255
+    kernel = np.ones((5,5),np.uint8)
+
+    for i in range(output.shape[0]):
+        path = input_path[i].split('/')
+        output_folder = os.path.join(args.output_root, path[-2])
+        try:
+            os.mkdir(output_folder)
+        except:
+            pass
+        output_path = os.path.join(output_folder, path[-1])
+        if overlap:
+            img = cv2.morphologyEx(output[i].astype(np.uint8), cv2.MORPH_OPEN, kernel)
+            img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+            img = np.expand_dims(img, axis=2)
+            zeros = np.zeros(img.shape)
+            img = np.concatenate((zeros,zeros,img), axis=2)
+            img = np.array(img).astype(np.float32)
+            img = input[i] + img
+            if img.max() > 0:
+                img = (img/img.max())*255
+            else:
+                img = (img/1) * 255
+            cv2.imwrite(output_path, img)
+        else:
+            img = output[i]
+            cv2.imwrite(output_path, img)
+
+class Checkpoint:
+    def __init__(self, model, optimizer=None, epoch=0, best_score=1):
+        self.model = model
+        self.optimizer = optimizer
+        self.epoch = epoch
+        self.best_score = best_score
+
+    def load(self, path):
+        checkpoint = torch.load(path)
+        self.model.load_state_dict(checkpoint["model_state"])
+        self.epoch = checkpoint["epoch"]
+        self.best_score = checkpoint["best_score"]
+        #if self.optimizer:
+        #    self.optimizer.load_state_dict(checkpoint["optimizer_state"])
+
+    def save(self, path):
+        state_dict = self.model.module.state_dict()
+        torch.save({"model_state": state_dict,
+                    "optimizer_state": self.optimizer.state_dict(),
+                    "epoch": self.epoch,
+                    "best_score": self.best_score}, path)
+
+
+
+def progress_bar(current, total, msg=None):
+    ''' Source Code from 'kuangliu/pytorch-cifar'
+        (https://github.com/kuangliu/pytorch-cifar/blob/master/utils.py)
+    '''
+    global last_time, begin_time
+    if current == 0:
+        begin_time = time.time()  # Reset for new bar.
+
+    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+
+    sys.stdout.write(' [')
+    for i in range(cur_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+
+    cur_time = time.time()
+    step_time = cur_time - last_time
+    last_time = cur_time
+    tot_time = cur_time - begin_time
+
+    L = []
+    L.append('  Step: %s' % format_time(step_time))
+    L.append(' | Tot: %s' % format_time(tot_time))
+    if msg:
+        L.append(' | ' + msg)
+
+    msg = ''.join(L)
+    sys.stdout.write(msg)
+    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+        sys.stdout.write(' ')
+
+    # Go back to the center of the bar.
+    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
+        sys.stdout.write('\b')
+    sys.stdout.write(' %d/%d ' % (current+1, total))
+
+    if current < total-1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+def format_time(seconds):
+    ''' Source Code from 'kuangliu/pytorch-cifar'
+        (https://github.com/kuangliu/pytorch-cifar/blob/master/utils.py)
+    '''
+    days = int(seconds / 3600/24)
+    seconds = seconds - days*3600*24
+    hours = int(seconds / 3600)
+    seconds = seconds - hours*3600
+    minutes = int(seconds / 60)
+    seconds = seconds - minutes*60
+    secondsf = int(seconds)
+    seconds = seconds - secondsf
+    millis = int(seconds*1000)
+
+    f = ''
+    i = 1
+    if days > 0:
+        f += str(days) + 'D'
+        i += 1
+    if hours > 0 and i <= 2:
+        f += str(hours) + 'h'
+        i += 1
+    if minutes > 0 and i <= 2:
+        f += str(minutes) + 'm'
+        i += 1
+    if secondsf > 0 and i <= 2:
+        f += str(secondsf) + 's'
+        i += 1
+    if millis > 0 and i <= 2:
+        f += str(millis) + 'ms'
+        i += 1
+    if f == '':
+        f = '0ms'
+    return f
+
