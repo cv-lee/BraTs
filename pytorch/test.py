12778301991e6bcd55d725882fe46d5f104f0def
Joohyun Lee <dlee110600@gmail.com>
2018-10-06 02:11:55 +0900

    Update test.py

--- pytorch/test.py
+++ pytorch/test.py
@@ -12,6 +12,7 @@ from torch.optim import SGD
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
+import config
 from dataset import *
 from models import *
 from utils import *
@@ -20,20 +21,21 @@ from utils import *
 def test(args):
 
     # Device Init
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = config.device
     cudnn.benchmark = True
 
     # Data Load
-    testloader = data_loader(args, mode='TEST')
+    testloader = data_loader(args, mode='test')
     # Model Load
-    unet, optimizer, best_score, start_epoch = load_model(args, class_num=2, mode='TEST')
+    model, optimizer, best_score, start_epoch = \
+        load_model(args, class_num=config.class_num, mode='test')
 
     # Loss Init
     unet.eval()
-    for idx, (input, input_path) in enumerate(testloader):
-        input = input.to(device)
-        output = unet(input)
-        save_img(args, input, output, input_path)
+    for idx, (inputs, paths) in enumerate(testloader):
+        inputs = inputs.to(device)
+        outputs = unet(inputs)
+        save_img(args, inputs, outputs, paths)
 
 
 if __name__ == "__main__":
@@ -42,7 +44,7 @@ if __name__ == "__main__":
                         help="Model Trianing resume.")
     parser.add_argument("--batch_size", type=int, default=16,
                         help="The batch size to load the data")
-    parser.add_argument("--data", type=str, default="COMPLETE",
+    parser.add_argument("--data", type=str, default="complete",
                         help="Label data type.")
     parser.add_argument("--img_root", type=str, default="../data/ImagesTr",
                         help="The directory containing the training image dataset.")
