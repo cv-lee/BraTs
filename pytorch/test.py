a8b566c7a63bf60a77bdef0793e3caae0e763888
Joohyun Lee <dlee110600@gmail.com>
2018-10-06 02:56:28 +0900

    Update test.py

--- pytorch/test.py
+++ pytorch/test.py
@@ -1,18 +1,9 @@
-import shutil
 import argparse
 import pdb
 
-import numpy as np
-import torch
-import torch.nn as nn
 import torch.backends.cudnn as cudnn
-import torch.nn.functional as F
 
-from torch.optim import SGD
-from torchvision import transforms
-from torch.utils.data import DataLoader
-
-import config
+from config import *
 from dataset import *
 from models import *
 from utils import *
@@ -26,10 +17,11 @@ def test(args):
 
     # Data Load
     testloader = data_loader(args, mode='test')
-    # Model Load
-    model, optimizer, best_score, start_epoch = \
-        load_model(args, class_num=config.class_num, mode='test')
 
+    # Model Load
+    model, _, _, _ = load_model(args, class_num=config.class_num, mode='test')
+
+    # Loss Init
     unet.eval()
     for idx, (inputs, paths) in enumerate(testloader):
         inputs = inputs.to(device)
@@ -39,8 +31,6 @@ def test(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--resume", type=bool, default=True,
-                        help="Model Trianing resume.")
     parser.add_argument("--batch_size", type=int, default=16,
                         help="The batch size to load the data")
     parser.add_argument("--data", type=str, default="complete",
