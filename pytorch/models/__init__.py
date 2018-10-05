1ed7f482c3813552727670dfb9c6ce004afcc970
Joohyun Lee <dlee110600@gmail.com>
2018-10-06 02:58:44 +0900

    Update __init__.py

--- pytorch/models/__init__.py
+++ pytorch/models/__init__.py
@@ -5,22 +5,27 @@ import torch
 from torch.optim import Adam
 
 from .unet import *
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from utils import *
-
 import config
 
+
 def load_model(args, class_num, mode):
+
     device = config.device
     model = UNet(class_num)
+
     if mode == 'train':
+        resume = args.resume
         optimizer = Adam(model.parameters(), lr=args.lr)
     elif mode == 'test':
+        resume = True
         optimizer = None
     else:
-        raise ValueError('InValid Flag in load_model')
+        raise ValueError('load_model mode ERROR')
 
-    if args.resume:
+    if resume:
         checkpoint = Checkpoint(model, optimizer)
         checkpoint.load(args.ckpt_path)
         best_score = checkpoint.best_score
@@ -33,4 +38,5 @@ def load_model(args, class_num, mode):
         model.cuda()
         model = torch.nn.DataParallel(model)
         torch.backends.cudnn.benchmark=True
+
     return model, optimizer, best_score, start_epoch
