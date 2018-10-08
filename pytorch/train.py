dd0cd82c34ab6dfcd5982243aa13279cd580046e
Joohyun Lee <dlee110600@gmail.com>
2018-10-08 20:55:50 +0900

    Update train.py

--- pytorch/train.py
+++ pytorch/train.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import pdb
 
 import torch
@@ -12,9 +13,10 @@ from utils import *
 
 def train(args):
 
-    # Device Init
+    # Variables Init
     device = config.device
     cudnn.benchmark = True
+    get_logger()
 
     # Data Load
     trainloader = data_loader(args, mode='train')
@@ -24,13 +26,13 @@ def train(args):
     model, optimizer, best_score, start_epoch =\
         load_model(args, class_num=config.class_num, mode='train')
 
-    for epoch in range(start_epoch, args.epochs + 1):
+    for epoch in range(start_epoch, start_epoch+args.epochs):
 
         # Train Model
         print('\nEpoch: {}\n<Train>\n'.format(epoch))
         model.train(True)
         loss = 0
-        lr = args.lr * (0.5 ** (epoch // 5))
+        lr = args.lr * (0.5 ** (epoch // 3))
         for param_group in optimizer.param_groups:
             param_group["lr"] = lr
         torch.set_grad_enabled(True)
@@ -44,6 +46,9 @@ def train(args):
             loss += float(batch_loss)
             progress_bar(idx, len(trainloader), 'Loss: %.5f, Dice-Coef: %.5f'
                          %((loss/(idx+1)), (1-(loss/(idx+1)))))
+        log_msg = '\n'.join(['Epoch: %d  Loss: %.5f,  Dice-Coef:  %.5f'\
+                         %(epoch, loss/(idx+1), 1-(loss/(idx+1)))])
+        logging.info(log_msg)
 
         # Validate Model
         print('\n<Validation>\n')
@@ -53,10 +58,15 @@ def train(args):
         for idx, (inputs, targets, paths) in enumerate(validloader):
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = model(inputs)
-            batch_loss = dice_coef(outputs, targets)
+            #outputs = post_process(args, inputs, outputs, save=False)
+            batch_loss = dice_coef(outputs, targets, backprop=False)
             loss += float(batch_loss)
             progress_bar(idx, len(validloader), 'Loss: %.5f, Dice-Coef: %.5f'
                          %((loss/(idx+1)), (1-(loss/(idx+1)))))
+        log_msg = '\n'.join(['Epoch: %d  Loss: %.5f,  Dice-Coef:  %.5f'
+                        %(epoch, loss/(idx+1), 1-(loss/(idx+1)))])
+        logging.info(log_msg)
+
         loss /= (idx+1)
         score = 1 - loss
         if score > best_score:
@@ -72,7 +82,7 @@ if __name__ == "__main__":
                         help="Model Trianing resume.")
     parser.add_argument("--batch_size", type=int, default=52,
                         help="The batch size to load the data")
-    parser.add_argument("--epochs", type=int, default=100,
+    parser.add_argument("--epochs", type=int, default=10,
                         help="The training epochs to run.")
     parser.add_argument("--lr", type=float, default=0.0001,
                         help="Learning rate to use in training")
