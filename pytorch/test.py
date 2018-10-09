86fc84b5b1756979a1b05b99ad1adb62780a23a4
Joohyun Lee <dlee110600@gmail.com>
2018-10-09 18:01:16 +0900

    Update test.py

--- pytorch/test.py
+++ pytorch/test.py
@@ -19,19 +19,23 @@ def test(args):
     testloader = data_loader(args, mode='test')
 
     # Model Load
-    model, _, _, _ = load_model(args, class_num=config.class_num, mode='test')
+    net, _, _, _ = load_model(args, class_num=config.class_num, mode='test')
 
     # Loss Init
-    model.eval()
+    net.eval()
     torch.set_grad_enabled(False)
     for idx, (inputs, paths) in enumerate(testloader):
         inputs = inputs.to(device)
-        outputs = model(inputs)
+        outputs = net(inputs)
+        if type(outputs) == tuple:
+            outputs = outputs[0]
         post_process(args, inputs, outputs, paths)
         print(idx)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--model", type=str, default='pspnet', # Need to be fixed
+                        help="Model Name")
     parser.add_argument("--batch_size", type=int, default=155, # Need to be fixed
                         help="The batch size to load the data")
     parser.add_argument("--data", type=str, default="complete",
@@ -40,8 +44,8 @@ if __name__ == "__main__":
                         help="The directory containing the training image dataset.")
     parser.add_argument("--output_root", type=str, default="./output/prediction",
                         help="The directory containing the results.")
-    parser.add_argument("--ckpt_path", type=str, default="./checkpoint/model.tar",
-                        help="The directory containing the training label datgaset")
+    parser.add_argument("--ckpt_root", type=str, default="./checkpoint",
+                        help="The directory containing the trained model checkpoint")
     args = parser.parse_args()
 
     test(args)
