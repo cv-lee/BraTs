64d18b8bb07734ec79ced473f885975ca1c773c8
Joohyun Lee <dlee110600@gmail.com>
2018-10-09 17:20:12 +0900

    Update pspnet.py

--- pytorch/models/pspnet.py
+++ pytorch/models/pspnet.py
@@ -1,154 +1,21 @@
-from collections import OrderedDict
 import math
 import pdb
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils import model_zoo
-from torchvision.models.densenet import densenet121, densenet161
-from torchvision.models.squeezenet import squeezenet1_1
 from torch.nn.functional import softmax
 
-#import extractors
-#from extractors import *
 
-
-class PSPModule(nn.Module):
-    def __init__(self, features, out_features=1024, sizes=(1, 2, 3, 6)):
-        super().__init__()
-        self.stages = []
-        self.stages = nn.ModuleList([self._make_stage(features, size) for size in sizes])
-        self.bottleneck = nn.Conv2d(features * (len(sizes) + 1), out_features, kernel_size=1)
-        self.relu = nn.ReLU()
-
-    def _make_stage(self, features, size):
-        prior = nn.AdaptiveAvgPool2d(output_size=(size, size))
-        conv = nn.Conv2d(features, features, kernel_size=1, bias=False)
-        return nn.Sequential(prior, conv)
-
-    def forward(self, feats):
-        h, w = feats.size(2), feats.size(3)
-        priors = [F.upsample(input=stage(feats), size=(h, w), mode='bilinear') for stage in self.stages] + [feats]
-        bottle = self.bottleneck(torch.cat(priors, 1))
-        return self.relu(bottle)
-
-
-class PSPUpsample(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.PReLU()
-        )
-
-    def forward(self, x):
-        h, w = 2 * x.size(2), 2 * x.size(3)
-        p = F.upsample(input=x, size=(h, w), mode='bilinear')
-        return self.conv(p)
-
-
-class PSPNet(nn.Module):
-    def __init__(self, n_classes=2, sizes=(1, 2, 3, 6), psp_size=2048,
-                 deep_features_size=1024, backend='resnet34',
-                 pretrained=True):
-        super().__init__()
-        if backend =='resnet18':
-            self.feats = resnet18()
-        elif backend == 'resnet34':
-            self.feats = resnet34()
-        elif backend == 'resnet50':
-            self.feats = resnet50()
-        elif backend == 'resnet101':
-            self.feats = resnet101()
-        elif backend == 'resnet151':
-            self.feats = resnet151()
-        elif backend == 'squeezenet':
-            self.feats = squeezenet()
-        else:
-            raise ValueError('backend ERROR')
-        #self.feats = getattr(extractors, backend)(pretrained)
-        self.psp = PSPModule(psp_size, 1024, sizes)
-        self.drop_1 = nn.Dropout2d(p=0.3)
-
-        self.up_1 = PSPUpsample(1024, 256)
-        self.up_2 = PSPUpsample(256, 64)
-        self.up_3 = PSPUpsample(64, 64)
-
-        self.drop_2 = nn.Dropout2d(p=0.15)
-        self.final = nn.Sequential(
-            nn.Conv2d(64, n_classes, kernel_size=1),
-            nn.LogSoftmax()
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(deep_features_size, 256),
-            nn.ReLU(),
-            nn.Linear(256, n_classes)
-        )
-
-    def forward(self, x):
-        #print(x.shape)
-        f, class_f = self.feats(x)
-        #print(f.shape)
-        p = self.psp(f)
-        #print(p.shape)
-        p = self.drop_1(p)
-        #print(p.shape)
-        p = self.up_1(p)
-        p = self.drop_2(p)
-        #print(p.shape)
-        p = self.up_2(p)
-        p = self.drop_2(p)
-        #print(p.shape)
-        p = self.up_3(p)
-        p = self.drop_2(p)
-        #print(p.shape)
-        auxiliary = F.adaptive_max_pool2d(input=class_f, output_size=(1, 1)).view(-1, class_f.size(1))
-        #print(auxiliary.shape)
-        #print(self.final(p).shape)
-        p = self.final(p)
-        p = softmax(p, dim=1)
-        return p, self.classifier(auxiliary)
-
-def test():
-    # SqueezeNet
-    #model = PSPNet(sizes=(1,2,3,6), psp_size=512, deep_features_size=256, backend='squeezenet')
-    pdb.set_trace()
-    # ResNet50
-    net = PSPNet(sizes=(1,2,3,6), psp_size=2048, deep_features_size=1024, backend='resnet50')
-    net = nn.DataParallel(net)
-    net.cuda()
-
-    x = torch.randn(1,3,1024,1024)
-    y = net(x)
-    print(y.size)
-
-#test()
-
-
-
-
-
-
-def load_weights_sequential(target, source_state):
-    new_dict = OrderedDict()
-    for (k1, v1), (k2, v2) in zip(target.state_dict().items(), source_state.items()):
-        new_dict[k1] = v2
-    target.load_state_dict(new_dict)
+#############################################
+#                                           #
+#       PSPNet Backbone Networks            #
+#                                           #
+#############################################
 
 '''
-    Implementation of dilated ResNet-101 with deep supervision. Downsampling is changed to 8x
+    Implementation of dilated ResNet with deep supervision. Downsampling is changed to 8x
 '''
-model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-}
-
 
 def conv3x3(in_planes, out_planes, stride=1, dilation=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
@@ -279,106 +146,8 @@ class ResNet(nn.Module):
 
 
 '''
-    Implementation of DenseNet with deep supervision. Downsampling is changed to 8x 
+    Implementation of Squeeze with deep supervision. Downsampling is changed to 8x
 '''
-
-
-class _DenseLayer(nn.Sequential):
-    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
-        super(_DenseLayer, self).__init__()
-        self.add_module('norm.1', nn.BatchNorm2d(num_input_features)),
-        self.add_module('relu.1', nn.ReLU(inplace=True)),
-        self.add_module('conv.1', nn.Conv2d(num_input_features, bn_size *
-                                            growth_rate, kernel_size=1, stride=1, bias=False)),
-        self.add_module('norm.2', nn.BatchNorm2d(bn_size * growth_rate)),
-        self.add_module('relu.2', nn.ReLU(inplace=True)),
-        self.add_module('conv.2', nn.Conv2d(bn_size * growth_rate, growth_rate,
-                                            kernel_size=3, stride=1, padding=1, bias=False)),
-        self.drop_rate = drop_rate
-
-    def forward(self, x):
-        new_features = super(_DenseLayer, self).forward(x)
-        if self.drop_rate > 0:
-            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
-        return torch.cat([x, new_features], 1)
-
-
-class _DenseBlock(nn.Sequential):
-    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate):
-        super(_DenseBlock, self).__init__()
-        for i in range(num_layers):
-            layer = _DenseLayer(num_input_features + i * growth_rate, growth_rate, bn_size, drop_rate)
-            self.add_module('denselayer%d' % (i + 1), layer)
-
-
-class _Transition(nn.Sequential):
-    def __init__(self, num_input_features, num_output_features, downsample=True):
-        super(_Transition, self).__init__()
-        self.add_module('norm', nn.BatchNorm2d(num_input_features))
-        self.add_module('relu', nn.ReLU(inplace=True))
-        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
-                                          kernel_size=1, stride=1, bias=False))
-        if downsample:
-            self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
-        else:
-            self.add_module('pool', nn.AvgPool2d(kernel_size=1, stride=1))  # compatibility hack
-
-
-class DenseNet(nn.Module):
-    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
-                 num_init_features=64, bn_size=4, drop_rate=0, pretrained=True):
-
-        super(DenseNet, self).__init__()
-
-        # First convolution
-        self.start_features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
-
-        # Each denseblock
-        num_features = num_init_features
-
-        init_weights = list(densenet121(pretrained=True).features.children())
-        start = 0
-        for i, c in enumerate(self.start_features.children()):
-            if pretrained:
-                c.load_state_dict(init_weights[i].state_dict())
-            start += 1
-        self.blocks = nn.ModuleList()
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
-                                bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
-            if pretrained:
-                block.load_state_dict(init_weights[start].state_dict())
-            start += 1
-            self.blocks.append(block)
-            setattr(self, 'denseblock%d' % (i + 1), block)
-
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                downsample = i < 1
-                trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2,
-                                    downsample=downsample)
-                if pretrained:
-                    trans.load_state_dict(init_weights[start].state_dict())
-                start += 1
-                self.blocks.append(trans)
-                setattr(self, 'transition%d' % (i + 1), trans)
-                num_features = num_features // 2
-
-    def forward(self, x):
-        out = self.start_features(x)
-        deep_features = None
-        for i, block in enumerate(self.blocks):
-            out = block(out)
-            if i == 5:
-                deep_features = out
-
-        return out, deep_features
-
 
 class Fire(nn.Module):
 
@@ -405,11 +174,11 @@ class Fire(nn.Module):
 
 class SqueezeNet(nn.Module):
 
-    def __init__(self, pretrained=False):
+    def __init__(self):
         super(SqueezeNet, self).__init__()
 
         self.feat_1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1),
             nn.ReLU(inplace=True)
         )
         self.feat_2 = nn.Sequential(
@@ -428,9 +197,6 @@ class SqueezeNet(nn.Module):
             Fire(384, 64, 256, 256, 4),
             Fire(512, 64, 256, 256, 4)
         )
-        if pretrained:
-            weights = squeezenet1_1(pretrained=True).features.state_dict()
-            load_weights_sequential(self, weights)
 
     def forward(self, x):
         f1 = self.feat_1(x)
@@ -449,10 +215,6 @@ def squeezenet():
     return SqueezeNet()
 
 
-def densenet():
-    return DenseNet()
-
-
 def resnet18():
     model = ResNet(BasicBlock, [2, 2, 2, 2])
     return model
@@ -468,11 +230,135 @@ def resnet50():
     return model
 
 
-def resnet101():
-    model = ResNet(Bottleneck, [3, 4, 23, 3])
-    return model
+
+#####################################################
+#										            #
+#                   PSPNet							#
+#										            #
+#####################################################
 
 
-def resnet152():
-    model = ResNet(Bottleneck, [3, 8, 36, 3])
-    return model
+class PSPModule(nn.Module):
+    def __init__(self, features, out_features=1024, sizes=(1, 2, 3, 6)):
+        super().__init__()
+        self.stages = []
+        self.stages = nn.ModuleList([self._make_stage(features, size) for size in sizes])
+        self.bottleneck = nn.Conv2d(features * (len(sizes) + 1), out_features, kernel_size=1)
+        self.relu = nn.ReLU()
+
+    def _make_stage(self, features, size):
+        prior = nn.AdaptiveAvgPool2d(output_size=(size, size))
+        conv = nn.Conv2d(features, features, kernel_size=1, bias=False)
+        return nn.Sequential(prior, conv)
+
+    def forward(self, feats):
+        h, w = feats.size(2), feats.size(3)
+        priors = [F.interpolate(input=stage(feats), size=(h, w),\
+                 mode='bilinear', align_corners=True) for stage in self.stages] + [feats]
+        bottle = self.bottleneck(torch.cat(priors, 1))
+        return self.relu(bottle)
+
+
+class PSPUpsample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.PReLU()
+        )
+
+    def forward(self, x):
+        h, w = 2 * x.size(2), 2 * x.size(3)
+        p = F.interpolate(input=x, size=(h, w), mode='bilinear', align_corners=True)
+        return self.conv(p)
+
+
+class PSPNet(nn.Module):
+    def __init__(self, n_classes=2, sizes=(1, 2, 3, 6), psp_size=2048,
+                 deep_features_size=1024, backend='resnet34',
+                 pretrained=True):
+        super().__init__()
+        if backend =='resnet18':
+            self.feats = resnet18()
+        elif backend == 'resnet34':
+            self.feats = resnet34()
+        elif backend == 'resnet50':
+            self.feats = resnet50()
+        elif backend == 'resnet101':
+            self.feats = resnet101()
+        elif backend == 'resnet152':
+            self.feats = resnet152()
+        elif backend == 'squeezenet':
+            self.feats = squeezenet()
+        else:
+            raise ValueError('backend ERROR')
+        #self.feats = getattr(extractors, backend)(pretrained)
+        self.psp = PSPModule(psp_size, 1024, sizes)
+        self.drop_1 = nn.Dropout2d(p=0.3)
+
+        self.up_1 = PSPUpsample(1024, 256)
+        self.up_2 = PSPUpsample(256, 64)
+        self.up_3 = PSPUpsample(64, 64)
+
+        self.drop_2 = nn.Dropout2d(p=0.15)
+        self.final = nn.Sequential(
+            nn.Conv2d(64, n_classes, kernel_size=1),
+            nn.LogSoftmax()
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(deep_features_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, n_classes)
+        )
+
+    def forward(self, x):
+        f, class_f = self.feats(x)
+        p = self.psp(f)
+        p = self.drop_1(p)
+        p = self.up_1(p)
+        p = self.drop_2(p)
+        p = self.up_2(p)
+        p = self.drop_2(p)
+        p = self.up_3(p)
+        p = self.drop_2(p)
+        auxiliary = F.adaptive_max_pool2d(input=class_f, output_size=(1, 1)).\
+                    view(-1, class_f.size(1))
+        p = self.final(p)
+        p = softmax(p, dim=1)
+        return p, self.classifier(auxiliary)
+
+
+def pspnet_squeeze():
+    return PSPNet(sizes=(1,2,3,6), psp_size=512, deep_features_size=256, backend='squeezenet')
+
+def pspnet_res18():
+    return PSPNet(sizes=(1,2,3,6), psp_size=512, deep_features_size=256, backend='resnet18')
+
+def pspnet_res34():
+    return PSPNet(sizes=(1,2,3,6), psp_size=512, deep_features_size=256, backend='resnet34')
+
+def pspnet_res50():
+    return PSPNet(sizes=(1,2,3,6), psp_size=2048, deep_features_size=1024, backend='resnet50')
+
+def test():
+    # SqueezeNet
+    #net = pspnet_squeeze()
+    # Resnet18
+    #net = pspnet_res18()
+    # Resnet34
+    #net = pspnet_res34()
+    # Resnet50
+    net = pspnet_res50()
+
+    net = nn.DataParallel(net)
+    net.cuda()
+
+    x = torch.randn(1,1,1024,1024)
+    y1, y2 = net(x)
+    print(y1.size())
+    print(y2.size())
+
+
+#test()
