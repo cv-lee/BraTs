14ee55c69c381f774533e8ac500a0db7b88310f4
Joohyun Lee <dlee110600@gmail.com>
2018-11-03 10:59:35 +0900

    Update deeplab.py

--- pytorch/models/deeplab.py
+++ pytorch/models/deeplab.py
@@ -144,8 +144,8 @@ class ResNet(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0], drop_rate=drop_rate)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, drop_rate)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, drop_rate)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, drop_rate=drop_rate)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, drop_rate=drop_rate)
 
         self.low_conv = nn.Conv2d(64*block.expansion, 64, kernel_size=3,
                                   stride=1, padding=1, bias=False)
